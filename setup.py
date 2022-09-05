7f4eb6f712ea99087a336aed775b1b2bc7c2863c
Hsiao <Jason.Hsiao@acer.com>
2022-09-05 10:10:00 +0100

    change names to use pep503 normalized names (. become -)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from jhsiao.namespace import make_ns, fdir
 
 make_ns('jhsiao', dir=fdir(__file__))
 setup(
-    name='jhsiao.utils',
+    name='jhsiao-utils',
     version='0.0.1',
     author='Jason Hsiao',
     author_email='oaishnosaj@gmail.com',
